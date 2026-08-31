41ca0dc8b81905b128792f04b0c268ae5806e0e6
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-08-31 01:43:52 -0300

    Actualiza texto del pie de página

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,1 @@
-export function Footer(){return <footer className="site-footer"><div className="container footer-bottom"><span>© 2026 Gestoría AyS. Todos los derechos reservados.</span></div></footer>}
+export function Footer(){return <footer className="site-footer"><div className="container footer-bottom"><span>© 2026 - Todos los derechos reservados.</span></div></footer>}
